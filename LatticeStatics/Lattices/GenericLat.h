011d13fc87d25f450d90fcad78574f8ec9d0d781
elliottr <elliottr>
2002-04-30 13:12:40 +0000

    Generic Lattice virtual class to allow modes to be more generic

---
+++ LatticeStatics/Lattices/GenericLat.h
@@ -0,0 +1,20 @@
+#ifndef __GenericLat
+#define __GenericLat
+
+#include "Lattice.h"
+
+class GenericLat : public Lattice
+{
+public:
+   virtual ~GenericLat() {}
+
+   virtual Vector DOF() = 0;
+   virtual void SetDOF(const Vector &dof) = 0;
+   enum TDeriv {T0,DT};
+   virtual Matrix StressDT() = 0;
+   virtual double Temp() = 0;
+   virtual void SetTemp(const double &temp) = 0;
+};
+
+#endif
+   
