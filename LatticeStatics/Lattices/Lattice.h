95c26b9f92976b32e9097d34c033086d40dde8f6
elliottr <elliottr>
2002-09-12 21:21:10 +0000

    added to lattice interface and made some updates

--- LatticeStatics/Lattices/Lattice.h
+++ LatticeStatics/Lattices/Lattice.h
@@ -13,11 +13,17 @@ public:
    virtual ~Lattice() {}
 
    virtual double Energy() = 0;
+   double E0() {return Energy();}
    virtual Matrix Stress() = 0;
+   Matrix E1() {return Stress();}
    virtual Matrix Stiffness() = 0;
+   Matrix E2() {return Stiffness();}
    virtual Matrix Moduli() = 0;
-   virtual int StiffnessNulity(double *Min=NULL) = 0;
-   virtual void CriticalPointInfo(double Tolerance,int Width,ostream &out) = 0;
+   virtual Matrix E3() = 0;
+   virtual Matrix E4() = 0;
+   virtual int StiffnessNulity(double *Min=NULL);
+   virtual void CriticalPointInfo(const Vector &DrDt,double Tolerance,
+				  char *datafile,int Width,ostream &out) = 0;
 
    enum PrintDetail {PrintLong,PrintShort};
    virtual void Print(ostream &out,PrintDetail flag) = 0;
