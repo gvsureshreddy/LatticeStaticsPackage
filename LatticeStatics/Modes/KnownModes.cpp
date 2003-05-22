8fe535e691d5d98286363f6bf841230764bc55ff
elliottr <elliottr>
2003-05-22 18:39:20 +0000

    added mode B19

--- LatticeStatics/Modes/KnownModes.cpp
+++ LatticeStatics/Modes/KnownModes.cpp
@@ -8,7 +8,8 @@ LatticeMode *InitializeMode(Lattice *Lat,char *datafile,const char *prefix)
    const char *Modes[]={"UniDefTemp2DExpand",
 			"ML_Expand",
 			"ML_NaCl",
-			"ML_Rhombo"};
+			"ML_Rhombo",
+			"ML_B19"};
 
    switch (GetStringParameter(prefix,"MainModeType",datafile,Modes,NoModes))
    {
@@ -30,6 +31,11 @@ LatticeMode *InitializeMode(Lattice *Lat,char *datafile,const char *prefix)
       case 3:
       {
 	 return new ML_Rhombo(Lat);
+      }
+      break;
+      case 4:
+      {
+	 return new ML_B19(Lat);
       }
       break;
       case -1:
--- LatticeStatics/Modes/KnownModes.h
+++ LatticeStatics/Modes/KnownModes.h
@@ -5,8 +5,9 @@
 #include "ML_Expand.h"
 #include "ML_NaCl.h"
 #include "ML_Rhombo.h"
+#include "ML_B19.h"
 
-#define KNOWNMODES 4
+#define KNOWNMODES 5
 
 LatticeMode *InitializeMode(Lattice *Lat,char *datafile,const char *prefix);
 
