87931902c1278c5078f9e5edfe644065ba2c5cdf
elliottr <elliottr>
2003-08-08 17:51:16 +0000

    program to do blochwave calc give lattice config.

---
+++ LatticeStatics/Utility/BlochWave.cpp
@@ -0,0 +1,64 @@
+#include "KnownLattices.h"
+#include "UtilityFunctions.h"
+#include <fstream.h>
+
+void GetMainSettings(int &Width,int &Presision,char *datafile,const char *prefix);
+
+int main(int argc,char *argv[])
+{
+   // Check commandline args
+   if (argc < 3)
+   {
+      cerr << "Usage: " << argv[0]
+	   << " ParamFile GridSize Echo" << endl;
+      cerr << "Built on:               " << builddate() << endl
+	   << "LinearAlgebra Built on: " << LinearAlgebraBuildDate() << endl
+	   << "MyMath Built on:        " << MyMathBuildDate() << endl;
+      exit(-1);
+   }
+
+   char *datafile = argv[1],
+      prefix[LINELENGTH];
+   int GridSize = atoi(argv[2]);
+
+   strcpy(prefix,"^Input File:");
+   
+   Lattice *Lat;
+
+   int Width,Precision,Echo=0;
+
+   if (argc == 4) Echo = 1;
+
+   GetMainSettings(Width,Precision,datafile,prefix);
+
+   Lat = InitializeLattice(datafile,prefix,Echo);
+   Lat->SetGridSize(GridSize);
+
+   cout  << setiosflags(ios::fixed) << setprecision(Precision);
+
+   Vector DOF((Lat->DOF()).Dim()),Y(3);
+   int BlochWaveStable;
+   double Temp;
+   
+   while (cin >> Temp)
+   {
+      cin >> DOF;
+
+      Lat->SetTemp(Temp);
+      Lat->SetDOF(DOF);
+
+      BlochWaveStable = Lat->BlochWave(Y);
+
+      cout << "BlochWave Stability (GridSize=" << GridSize << "):"
+	   << setw(Width) << BlochWaveStable << ", "
+	   << setw(Width) << Y << endl;
+   }
+   
+   return 1;
+}
+
+void GetMainSettings(int &Width, int &Precision,char *datafile,const char *prefix)
+{
+   if(!GetParameter(prefix,"MainFieldWidth",datafile,"%d",&Width)) exit(-1);
+   if(!GetParameter(prefix,"MainPrecision",datafile,"%d",&Precision)) exit(-1);   
+}
