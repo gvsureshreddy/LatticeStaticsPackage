0dbc3b5e007763870613d82058ce4e61d8ddce95
elliottr <elliottr>
2003-09-20 04:53:43 +0000

    removed the volume option and made the points consistant with the finite fourier transform.  Note: when only using half the cell I added 1 point in each direction to get to the zone boundary.

--- LatticeStatics/Utility/UnitCellIterator.h
+++ LatticeStatics/Utility/UnitCellIterator.h
@@ -7,24 +7,23 @@
 class UnitCellIterator
 {
 private:
-   // Cubic grid of size ((2^(GridSize_)) + 1)
+   // Cubic grid of size (GridSize_)^3
    int GridSize_;
    int VectorsLen_;
    double **Vectors_;
    int CurrentPOS_;
-   enum ScanType {Simple,Volume};
    
-   void Initialize(int GridSize,ScanType ScnTyp,int DoHalfOnly,int SkipZero);
+   void Initialize(int GridSize,int DoHalfOnly,int SkipZero);
 
 public:
 
    UnitCellIterator(): Vectors_(NULL) {}
-   UnitCellIterator(int GridSize,ScanType ScnTyp=Simple,int DoHalfOnly=1,int SkipZero=1)
-      :Vectors_(NULL) {Initialize(GridSize,ScnTyp,DoHalfOnly,SkipZero);}
+   UnitCellIterator(int GridSize,int DoHalfOnly=1,int SkipZero=1)
+      :Vectors_(NULL) {Initialize(GridSize,DoHalfOnly,SkipZero);}
    ~UnitCellIterator();
 
-   void operator()(int GridSize,ScanType ScnTyp=Simple,int DoHalfOnly=1,int SkipZero=1)
-   {Initialize(GridSize,ScnTyp,DoHalfOnly,SkipZero);}
+   void operator()(int GridSize,int DoHalfOnly=1,int SkipZero=1)
+   {Initialize(GridSize,DoHalfOnly,SkipZero);}
 
    void Reset() {CurrentPOS_ = 0;}
 
