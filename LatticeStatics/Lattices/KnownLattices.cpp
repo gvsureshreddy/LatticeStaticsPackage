8f6d559a897a997cf3d8ec317808dbbc5f57c5c8
elliott <elliott>
2006-02-08 03:40:22 +0000

    added MultiChainTPP

--- LatticeStatics/Lattices/KnownLattices.cpp
+++ LatticeStatics/Lattices/KnownLattices.cpp
@@ -3,14 +3,18 @@
 
 Lattice *InitializeLattice(char *datafile,const char *prefix,int Echo,int Width,int Debug)
 {
-   const int NoLats = 1;
-   const char *Lattices[]={"MultiLatticeTPP"};
+   const int NoLats = 2;
+   const char *Lattices[]={"MultiLatticeTPP","MultiChainTPP"};
    
    switch (GetStringParameter(prefix,"MainLatticeType",datafile,Lattices,NoLats))
    {
       case 0:
       {
 	 return new MultiLatticeTPP(datafile,prefix,Echo,Width,Debug);
+      }
+      case 1:
+      {
+	 return new MultiChainTPP(datafile,prefix,Echo,Width,Debug);
       }
       case -1:
 	 {
