8eed6fc151c4fc936ee75f30fc44a59cf19d5715
elliott <elliott>
2008-07-27 20:15:35 +0000

    rewrote the Initialize() algorithm to more carefully deal with the z=0 plane to eliminate k points related by the inversion. Also, force GridSize to be odd.  Make sure the zone boundary is always in the list.

--- LatticeStatics/Utility/UnitCellIterator.cpp
+++ LatticeStatics/Utility/UnitCellIterator.cpp
@@ -4,17 +4,25 @@ void UnitCellIterator::Initialize(int const& GridSize,int const& DoHalfOnly,int 
 {
    GridSize_ = GridSize;
    CurrentPOS_ = 0;
-   
-   if ((GridSize_ < 2))
+   if (SkipZero > 1 || SkipZero < 0)
    {
-      cerr << "UnitCellIterator GridSize must be >= 2"
+      cerr << "UnitCellIterator SkipZero must be zero or one." << "\n";
+      exit(-1);
+   }
+   
+   if ((GridSize_ < 3) || !(GridSize_%2))
+   {
+      cerr << "UnitCellIterator GridSize must be > 2 and odd"
            << "\n";
       exit(-1);
    }
    VectorsLen_ = DoHalfOnly ?
-      GridSize*GridSize*(GridSize/2+1) - SkipZero
-      : GridSize*GridSize*GridSize - SkipZero;
-   
+      (GridSize_*GridSize_*(GridSize_/2) // Everything above z=0
+       + GridSize_*(GridSize_/2)         // z=0 Everything above y=0
+       + GridSize_/2+1                   // z=0 y=0 Everything from x=0 to x=GridSize/2+1
+       - SkipZero)                       // use (0,0,0) or not
+      : (GridSize_*GridSize_*GridSize_ - SkipZero); // Everything
+
    // Be extra sure there are no memory leaks
    if ( 0 != Vectors_)
    {
@@ -28,21 +36,51 @@ void UnitCellIterator::Initialize(int const& GridSize,int const& DoHalfOnly,int 
    {
       Vectors_[i] = Vectors_[i-1] + 3;
    }
-   
-   int even = 1-GridSize_%2;
-   for (int k= DoHalfOnly ? 0 : -GridSize_/2-(!DoHalfOnly&&even); k <= GridSize_/2; ++k)
-      for (int j=-GridSize_/2;j<=GridSize_/2-even;++j)
-         for (int i=-GridSize_/2;i<=GridSize_/2-even;++i)
+
+   double norm = 2.0*(GridSize_/2);
+   if (DoHalfOnly)
+   {
+      for (int i=(SkipZero?1:0);i<=GridSize_/2;++i)
+      {
+         Vectors_[CurrentPOS_][0] = i/norm;
+         Vectors_[CurrentPOS_][1] = Vectors_[CurrentPOS_][2] = 0.0;
+         ++CurrentPOS_;
+      }
+
+      for (int j=1;j<=GridSize_/2;++j)
+         for (int i=-GridSize_/2;i<=GridSize_/2;++i)
          {
-            if (!(SkipZero && (i == 0) && (j == 0) && (k == 0)))
+            Vectors_[CurrentPOS_][0] = i/norm;
+            Vectors_[CurrentPOS_][1] = j/norm;
+            Vectors_[CurrentPOS_][2] = 0.0;
+            ++CurrentPOS_;
+         }
+
+      for (int k=1;k<=GridSize_/2;++k)
+         for (int j=-GridSize_/2;j<=GridSize_/2;++j)
+            for (int i=-GridSize_/2;i<=GridSize_/2;++i)
             {
-               Vectors_[CurrentPOS_][0] = i/double(GridSize_);
-               Vectors_[CurrentPOS_][1] = j/double(GridSize_);
-               Vectors_[CurrentPOS_][2] = k/double(GridSize_);
+               Vectors_[CurrentPOS_][0] = i/norm;
+               Vectors_[CurrentPOS_][1] = j/norm;
+               Vectors_[CurrentPOS_][2] = k/norm;
                ++CurrentPOS_;
             }
-         }
-   
+   }
+   else
+   {
+      for (int k= -GridSize_/2; k <= GridSize_/2; ++k)
+         for (int j=-GridSize_/2;j<=GridSize_/2;++j)
+            for (int i=-GridSize_/2;i<=GridSize_/2;++i)
+            {
+               if (!(SkipZero && (i == 0) && (j == 0) && (k == 0)))
+               {
+                  Vectors_[CurrentPOS_][0] = i/norm;
+                  Vectors_[CurrentPOS_][1] = j/norm;
+                  Vectors_[CurrentPOS_][2] = k/norm;
+                  ++CurrentPOS_;
+               }
+            }
+   }
    CurrentPOS_ = 0;
 }
 
