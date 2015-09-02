ae8fc913c775e310176be720ed0f53a8bb4d2e57
Ryan S. Elliott <relliott@umn.edu>
2015-09-02 04:16:49 -0500

    Updated ExtraTestFunctions to deal with k=0 point

--- LatticeStatics/Lattices/FEAP.cpp
+++ LatticeStatics/Lattices/FEAP.cpp
@@ -1350,7 +1350,24 @@ void FEAP::ExtraTestFunctions(Vector& TF) const
          }
          else
          {
-           TF[k] = DkEigVal.MinElement();
+           double max = DkEigVal.MaxElement();
+           int NumZeroEig = 0;
+           if ((K_[0]==0.0) && (K_[1]==0.0))
+           {
+             for (int l=0; l < ndf_*(numnp_-nbn_/2); ++l)
+             {
+               if ((NumZeroEig < 2) && (fabs(DkEigVal[0][l]) < Tolerance_))
+               {
+                 DkEigVal[0][l] = max;
+                 ++NumZeroEig;
+               }
+             }
+             TF[k] = DkEigVal.MinElement();
+           }
+           else
+           {
+             TF[k] = DkEigVal.MinElement();
+           }
            ++k;
          }
          bloch_wave_out_ << setw(Width_) << K_[0]/(2*pi)
